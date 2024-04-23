d23ae4756c0ee7871c91ca09089ce8dfff03e4b5
Quyền Trần Văn <tranvanquyenx4@gmail.com>
2024-04-23 14:34:16 +0700

    fix sales admin

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -112,7 +112,7 @@
     </div>
     <footer class="footer">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">HRT © 2024. Premium <a
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">HRT Books © 2024. Premium <a
                     href="https://www.bootstrapdash.com/" target="_blank">Thiết kế bởi Tran Van Quyen</a></span>
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> <i
                     class="ti-heart text-danger ml-1"></i></span>
